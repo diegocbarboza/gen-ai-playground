0115873a12320597928b58fdbc19ac8405796082
Diego <diegocbarboza@gmail.com>
2026-04-15 22:34:27 -0300

    Cleaned requirements.txt and changed tracing to phoenix opensource

--- services/orchestrator.py
+++ services/orchestrator.py
@@ -1,29 +1,46 @@
 """Orchestrator class to manage interactions between different models, tools and agents."""
 import os
+import re
 from typing import TypedDict, List
 from dotenv import load_dotenv
-from arize.otel import register
+from phoenix.otel import register
 from openinference.instrumentation.langchain import LangChainInstrumentor
 #from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_core.messages import BaseMessage, SystemMessage
-from langchain_core.tools import tool
+from langchain_core.tools import Tool, tool
 from langchain_core.language_models import BaseChatModel
 from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode, tools_condition
+from api.agents import get_agent_list, get_agent_parameters
 
 load_dotenv()
 
-tracer_provider = register(
-    space_id=os.environ.get("ARIZE_SPACE_ID"),
-    api_key=os.environ.get("ARIZE_API_KEY"),
-    project_name=os.environ.get("ARIZE_PROJECT_NAME", "default"),
-)
+os.environ["PHOENIX_CLIENT_HEADERS"] = f"api_key={os.getenv('PHOENIX_API_KEY')}"
+tracer_provider = register()
+
 LangChainInstrumentor().instrument(tracer_provider=tracer_provider)
 
 @tool
 def get_weather(city: str) -> str:
     """Get the current weather for a city"""
     return f"The weather in {city} is sunny, 5°C."
+
+def make_tool(name: str, agent_parameters):
+    # Sanitize tool name: Replace invalid chars with underscores, ensure max length of 128
+    # Valid chars: alphanumeric (a-z, A-Z, 0-9), underscores (_), dots (.), colons (:), or dashes (-)
+    sanitized_name = re.sub(r'[^a-zA-Z0-9_.:-]', '_', name)[:128]
+    
+    #TODO: instantiate and run agent
+    def dynamic_tool(input: str) -> str:
+        # you can customize behavior here
+        print(f"Tool {sanitized_name} received input: {input}")
+        return f"[{sanitized_name}] received: {input}"
+
+    return Tool(
+        name=sanitized_name,
+        description=agent_parameters["description"],
+        func=dynamic_tool,
+    )
 
 class State(TypedDict):
     """Agent state."""
@@ -35,14 +52,21 @@ class Orchestrator:
 
     def __init__(self, model: BaseChatModel):
         self.model = model
+        self.tools = []
 
-
-    async def invoke(self, messages: list[BaseMessage]):
+    async def invoke(self, messages: list[BaseMessage], selected_agents: list[str] = None):
         """Invoke the orchestrator with the given user input."""
+    
+        #self.tools = [get_weather]
+        available_agents = get_agent_list()
+        for agent in selected_agents:
+            agent_params = get_agent_parameters(available_agents.index(agent))
+            self.tools.append(make_tool(agent, agent_params))
+
         graph = StateGraph(State)
 
         graph.add_node("tool_decider", self.tool_decider)
-        graph.add_node("tool_executor", ToolNode([get_weather]))
+        graph.add_node("tool_executor", ToolNode(self.tools))
         graph.add_node("final_answer", self.final_answer)
 
         graph.set_entry_point("tool_decider")
@@ -71,7 +95,7 @@ class Orchestrator:
 
     def tool_decider(self, state: State):
         """Node that decides if will use tools."""
-        llm_with_tools = self.model.bind_tools([get_weather])
+        llm_with_tools = self.model.bind_tools(self.tools)
 
         response = llm_with_tools.invoke(state["messages"])
 
